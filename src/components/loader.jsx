8d36ed9aea4b1a61292dd97561e972a9bc83c478
Himanshu <anshudhaliwal741@gmail.com>
2026-04-17 13:45:29 +0530

    Add Loader component for loading indication

--- src/components/loader.jsx
+++ src/components/loader.jsx
@@ -1,1 +1,8 @@
-
+export default function Loader() {
+  return (
+    <div className="loader-container">
+      <div className="loader-ring"></div>
+      <h3 className="mt-3">Loading PG Life...</h3>
+    </div>
+  );
+}
