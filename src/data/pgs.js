07037886dfbf6be6b2352e9e89645d36651aec9e
Himanshu <anshudhaliwal741@gmail.com>
2026-04-17 13:50:03 +0530

    Update pgs.js

--- src/data/pgs.js
+++ src/data/pgs.js
@@ -1,1 +1,117 @@
+export const pgsData = {
+  delhi: [
+    {
+      name: "Ashray Living PG",
+      address: "Amitabh Banquet, Bungalow Road, Kamla Nagar, Delhi-110007",
+      rent: "₹9500",
+      rating: 4.5,
+      images: ["/images/dh1.jpg", "/images/dh2.jpg"],
+      features: {
+        building: "Lift, Power Backup, Inverter",
+        common: "WiFi, TV, Purifier, Dining Table, Sofa, Washing Machine",
+        bedroom: "AC, 2 Beds, Mattress",
+        washroom: "Geyser"
+      }
+    },
+    {
+      name: "Comfort Rooms PG",
+      address: "metro (Near Made Easy, 9B, Lane No. 4, near kotak Mahindra ATM, IGNOU Rd, near saket, Saket, New Delhi, Delhi 110030",
+      rent: "₹8000",
+      rating: 4.0,
+      images: ["/images/dh3.jpg", "/images/dh4.jpg"],
+      features: {
+        building: "Lift, Security Guard",
+        common: "WiFi, Dining Table, Washing Machine",
+        bedroom: "Non-AC, 2 Beds",
+        washroom: "Geyser, Western Toilet"
+      }
+    },
+  ],
 
+  chandigarh: [
+    {
+      name: "Ganpati PG",
+      address: "SCO-4, Shivalik, Near Children Park, Near Gurudwara, Landran Road, Kharar, Anaj Mandi Road, Kharar - 140301",
+      rent: "₹8500",
+      rating: 5.0,
+      images: ["/images/ch1.jpg","/images/ch3.jpg"],
+      features: {
+        building: "Lift, Power Backup",
+        common: "WiFi, TV, Sofa",
+        bedroom: "AC, Single Bed",
+        washroom: "Geyser"
+      }
+    },
+    {
+      name: "Elite PG",
+      address: "Bhagoo Majra Phirni, Bhago Majra, Kharar, Punjab 140413.",
+      rent: "₹12000",
+      rating: 5.0,
+      images: ["/images/ch2.jpg","/images/ch4.jpg"],
+      features: {
+        building: "Lift, Inverter, CCTV",
+        common: "WiFi, Dining Table, Washing Machine, TV",
+        bedroom: "AC, 2 Beds, Mattress",
+        washroom: "Geyser, Shower"
+      }
+    },
+  ],
+
+  bengaluru: [
+    {
+      name: "Stanza Living PG",
+      address: "53, Hadosiddapura Village Varthur, Hobli, Sarjapura, Bengaluru, Karnataka 560035",
+      rent: "₹9000",
+      rating: 4.5,
+      images: ["/images/bg1.jpg","/images/bg2.jpg"],
+      features: {
+        building: "Lift, Power Backup, Security",
+        common: "WiFi, TV, Sofa, Washing Machine",
+        bedroom: "AC, 2 Beds",
+        washroom: "Geyser"
+      }
+    },
+    {
+      name: "Modern Deluxe PG",
+      address: "3rd cross, Patel Narayana Reddy Layout, No.7, 80 Feet Rd, near Bier Library, 6th Block, Koramangala, Bengaluru, Karnataka 560095",
+      rent: "₹8700",
+      rating: 4.0,
+      images: ["/images/bg3.jpg","/images/bg4.jpg"],
+      features: {
+        building: "Inverter, CCTV",
+        common: "WiFi, Purifier, Dining Table",
+        bedroom: "Non-AC, 2 Beds",
+        washroom: "Geyser, Bucket Setup"
+      }
+    },
+  ],
+
+  mohali: [
+    {
+      name: "The Ana House PG",
+      address: "sector 82 a, 1269, IT City Rd, Sahibzada Ajit Singh Nagar, Punjab 140306",
+      rent: "₹8500",
+      rating: 3.0,
+      images: ["/images/mh1.jpg","/images/mh2.jpg"],
+      features: {
+        building: "Lift, Power Backup",
+        common: "WiFi, TV, Sofa",
+        bedroom: "AC, 2 Beds",
+        washroom: "Geyser"
+      }
+    },
+    {
+      name: "Luxury PG 80",
+      address: "Sector 80, Mauli Baidwan, Sahibzada Ajit Singh Nagar, Punjab 140308",
+      rent: "₹9200",
+      rating: 4.5,
+       images: ["/images/mh3.jpg","/images/mh4.jpg"],
+      features: {
+        building: "Lift, CCTV, Security Guard, Power Backup",
+        common: "WiFi, TV, Dining Table, Sofa, Washing Machine",
+        bedroom: "AC, Queen Bed, Premium Mattress",
+        washroom: "Geyser, Shower Panel"
+      }
+    },
+  ],
+};
