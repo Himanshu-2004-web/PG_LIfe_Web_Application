211b0d1197d3877b5e98acdb72081cf407176967
Himanshu <anshudhaliwal741@gmail.com>
2026-04-17 14:01:51 +0530

    Set up React app with Bootstrap and Toastify

--- src/main.jsx
+++ src/main.jsx
@@ -1,1 +1,16 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
 
+import App from "./App";
+import "./style.css";
+
+import "bootstrap/dist/css/bootstrap.min.css";
+import * as bootstrap from "bootstrap/dist/js/bootstrap.bundle.min.js";
+import "react-toastify/dist/ReactToastify.css";
+
+// 🔥 VERY IMPORTANT
+window.bootstrap = bootstrap;
+
+ReactDOM
+  .createRoot(document.getElementById("root"))
+  .render(<App />);
